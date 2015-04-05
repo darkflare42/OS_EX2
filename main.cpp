e5785aae49b482b4d1deae838518b14f88719131
okeren <okeren@ubuntu>
2015-04-05 07:45:17 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: okeren
+ *
+ * Created on April 5, 2015, 7:38 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "test";
+    return 0;
+}
+

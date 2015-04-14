2f14c2a470787b6d2884ada37c1fec862a3cc651
odedex <karitvruda@hotmail.com>
2015-04-14 16:42:37 +0300

    Update PriorityQueue.h
    Added doxygen

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -16,15 +16,60 @@
 class PriorityQueue 
 {
 public:
+    /**
+    * Remove a shared_ptr from a queue, if it is in the Priority queue,
+    * otherwise does nothing.
+    * @param thread
+    * @return void
+    */
+    
+    
+    /**
+     * Standard constructor.
+     */
     PriorityQueue();
+    
+    /**
+     * Copy constructor.
+     */
     PriorityQueue(const PriorityQueue& orig);
+    
+    /**
+     * Standard destructor.
+     */
     virtual ~PriorityQueue();
     
+    /**
+    * Remove the first Thread * available in the queue, according to the priority.
+    * @return Pointer to the thread.
+    */
     Thread * pop();
-    void push(Thread * threadPtr);
+    
+    /**
+    * Remove a specific thread pointer from the queue.
+    * If the pointe is not in the queue, it does nothing.
+    * @param threadPtr a Thread pointer.
+    * @return void
+    */
     void pop(Thread * thread);
     
+    /**
+    * Push a thread pointer into the queue, according to its' priority.
+    * @param threadPtr a Thread pointer.
+    * @return void
+    */
+    void push(Thread * threadPtr);
+    
+    /**
+    * Check if the queue is empty.
+    * @return True iff the queue is empty.
+    */
     bool empty();
+    
+    /**
+    * Check the number of elements in the queue.
+    * @return Number of elements.
+    */
     int size();
     
 private:
